543bb1d9029c3ca328cb6e25fe514108cdf76e38
Jared Tucker <jtuc0006@student.monash.edu>
2025-01-11 15:23:09 +1100

    Add Endpoint for User Object Update

--- src/main/routes/colovia.ts
+++ src/main/routes/colovia.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { config } from "../config";
+import { AuthenticationError } from "../types/error.interface";
 import { UserProfile } from "../types/user.interface";
 
 const API_URL = config.coloviaApiURL;
@@ -22,6 +23,34 @@ export const coreDataManagementService = async (app: FastifyInstance) => {
             const { userId } = request.params;
 
             const response = await axios.get(`${API_URL}user/${userId}`);
+            reply.code(response.status).send(response.data);
+        }
+    );
+
+    app.put(
+        `/api/user/`,
+        async (
+            request: FastifyRequest<{ Body: UserProfile }>,
+            reply: FastifyReply
+        ) => {
+            //todo: Look into organisation/application roles to enable cross user updates
+            // For now, only the user can update their own profile
+
+            if (!request.user)
+                throw new AuthenticationError(
+                    "Authentication required to perform this action"
+                );
+
+            // Retrieve the ID of the user making the request
+            const { id } = request.user;
+            const user: UserProfile = request.body;
+
+            if (id !== user.userId)
+                throw new AuthenticationError(
+                    "You are not authorised to edit another user"
+                );
+
+            const response = await axios.put(`${API_URL}user/`, user);
             reply.code(response.status).send(response.data);
         }
     );
